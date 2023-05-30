887585d26fdadcce60c307630f96c92a55aaf965
PPSantos <pedro.pinto.santos@tecnico.ulisboa.pt>
2023-05-30 17:09:55 +0100

    Init.

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Python version = 3.8.10
+
+# Setup virtual environment and install libraries.
+virtualenv venv
+source venv/bin/activate
+pip3 install --upgrade pip
+pip3 install -r requirements.txt
+
+# Clone and install MPE.
+git clone https://github.com/PPSantos/multiagent-particle-envs.git
+cd multiagent-particle-envs
+pip3 install -e .
+cd ..
+
+# Clone and install lbforaging (v11 branch).
+git clone https://github.com/PPSantos/lb-foraging.git
+cd lb-foraging
+git checkout v11
+pip3 install -e .
+cd ..
+
+# Test installation.
+python3 src/main.py --config=iql_ns --env-config=gymma with env_args.key="SimpleSpreadXY-v0"
